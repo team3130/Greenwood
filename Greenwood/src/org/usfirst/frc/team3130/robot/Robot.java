eee596087dcf578830e44092b464607036347ba7
Duckfan77 <cwenthin@yahoo.com>
2016-10-01 12:28:20 -0500

    Created the Chassis Class
    Added the references to the navX file path so is useable, and then
    created and implemented Chassis.java based on the Chassis.h and .cpp in
    Geneva, duplicating the code into Java. Made all non-overriden methods
    and variable static, and then added a getInstance to allow access to
    systems that can't be static.

--- Greenwood/src/org/usfirst/frc/team3130/robot/Robot.java
+++ Greenwood/src/org/usfirst/frc/team3130/robot/Robot.java
@@ -5,8 +5,8 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team3130.robot.commands.ExampleCommand;
-import org.usfirst.frc.team3130.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team3130.robot.commands.*;
+import org.usfirst.frc.team3130.robot.subsystems.*;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -19,7 +19,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 
     Command autonomousCommand;
@@ -30,7 +29,10 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-		oi = new OI();
+		//Create instances of each class
+    	oi = new OI();
+		Chassis.GetInstance();
+		
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
